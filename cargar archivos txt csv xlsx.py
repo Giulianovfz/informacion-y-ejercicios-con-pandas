07a4e11d1c379067da97bb9f8bcddd993eca9839
Giulianovfz <giuliano.viacava.f@gmail.com>
2025-05-23 22:00:51 -0400

    se suben todos los documentos de pandas, archivos de python utilizando la librería y documentos complementarios en .txt y .xlsx

---
+++ cargar archivos txt csv xlsx.py
@@ -0,0 +1,18 @@
+import pandas
+
+# el sep se utiliza especificar el delimitador
+# de uso(separador) como por ej: ',' ';''.',etc
+data_frame = pandas.read_csv(
+    "Archivos csv txt xlsx y zip/supermarkets-semi-colons.txt", sep=';')
+
+print(f'\n\n{data_frame}')
+
+# se le incorpora el header para mostrar
+# el encabezado de las columnas
+data_frame1 = pandas.read_csv(
+    "Archivos csv txt xlsx y zip/supermarkets-semi-colons.txt", sep=';', header=None)
+
+print(f'\n\n{data_frame1}')
+
+# para automatizar //div[@class="card-title"]/a[contains(@href, "/entry/")]
+# https://www.vulnhub.com/
